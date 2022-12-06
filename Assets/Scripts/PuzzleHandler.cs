f0505d28a3dc8adc8c3a72aeed14810279f415a4
erikwtn <erik.wtn@gmail.com>
2022-12-06 11:48:59 +0000

    some code setup

--- Assets/Scripts/PuzzleHandler.cs
+++ Assets/Scripts/PuzzleHandler.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PuzzleHandler : MonoBehaviour
 {
+    public Transform[] pieceArray; // holds the current puzzle pieces
+    public Transform[] puzzleArray; // holds all the game puzzles
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < puzzleArray[0].childCount; i++)
+        {
+            pieceArray[i] = puzzleArray[i].GetChild(i);
+        }
     }
 
     // Update is called once per frame

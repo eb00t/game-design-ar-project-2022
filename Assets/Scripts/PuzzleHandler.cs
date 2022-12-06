cc8a98dd69faa6611466b9801c2c4f67c0ecc603
erikwtn <erik.wtn@gmail.com>
2022-12-06 12:29:10 +0000

    some code

--- Assets/Scripts/PuzzleHandler.cs
+++ Assets/Scripts/PuzzleHandler.cs
@@ -1,24 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UIElements;
 
 public class PuzzleHandler : MonoBehaviour
 {
-    public Transform[] pieceArray; // holds the current puzzle pieces
+    private List<Transform> pieceList; // holds the current puzzle pieces
     public Transform[] puzzleArray; // holds all the game puzzles
+
+    private Vector3 position;
+    private float width;
+    private float height;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < puzzleArray[0].childCount; i++)
-        {
-            pieceArray[i] = puzzleArray[i].GetChild(i);
-        }
+        GetChildren(puzzleArray[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // cycles through all children of specified puzzle and adds them to a list
+    private void GetChildren(Transform transform) 
+    {
+        foreach (Transform child in transform.gameObject.GetComponentsInChildren<Transform>())
+        {
+            if (child.GetComponent<MeshRenderer>())
+            {
+                pieceList.Add(child);
+            }  
+        }
+    }
+
+    private void SelectMovePiece()
+    {
+        Touch touch = Input.GetTouch(0);
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            Vector2 pos = touch.position;
+            pos.x = (pos.x - width) / width;
+            pos.y = (pos.y - height) / height;
+            position = new Vector3(-pos.x, pos.y, 0.0f);
+
+
+        }
     }
 }
